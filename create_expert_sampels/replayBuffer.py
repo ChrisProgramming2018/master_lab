f87b85b0cd621ae9912895ad60171632ddcebb7f
programmer <info2016frei@gmail.com>
2021-10-21 13:03:06 -0400

    add next state

--- create_expert_sampels/replayBuffer.py
+++ create_expert_sampels/replayBuffer.py
@@ -15,6 +15,7 @@ class ReplayBuffer(object):
         self.device = device
         self.obs_shape = obs_shape
         self.obses = np.empty((capacity, *obs_shape), dtype=np.uint8)
+        self.next_obses = np.empty((capacity, *obs_shape), dtype=np.uint8)
         self.actions = np.empty((capacity, *obs_shape), dtype=np.uint8)
         self.rewards = np.empty((capacity, *obs_shape), dtype=np.float32)
         self.dones = np.empty((capacity, *obs_shape), dtype=np.bool)
@@ -24,8 +25,9 @@ class ReplayBuffer(object):
     def __len__(self):
         return self.capacity if self.full else self.idx
 
-    def add(self, obs, action, reward, done):
+    def add(self, obs, next_obses, action, reward, done):
         np.copyto(self.obses[self.idx], obs.astype(np.uint8))
+        np.copyto(self.next_obses[self.idx], next_obses.astype(np.uint8))
         np.copyto(self.actions[self.idx], action.astype(np.uint8))
         np.copyto(self.rewards[self.idx], reward.astype(np.float32))
         np.copyto(self.dones[self.idx], done.astype(np.bool))
@@ -35,15 +37,17 @@ class ReplayBuffer(object):
     def sample(self, batch_size):
         idxs = np.random.randint(0, self.capacity if self.full else self.idx, size=batch_size)    
         obses = self.obses[idxs]
+        next_obses = self.next_obses[idxs]
         actions = self.actions[idxs]
         rewards = self.rewards[idxs]
         dones = self.dones[idxs]
         obses = torch.as_tensor(obses, device=self.device).float()
+        next_obses = torch.as_tensor(next_obses, device=self.device).float()
         actions = torch.as_tensor(actions, device=self.device).float()
         rewards = torch.as_tensor(rewards, device=self.device).float()
         dones = torch.as_tensor(dones, device=self.device).float()
         obses = torch.reshape(obses,(batch_size, 3, 256, 256))
-        return obses, actions, rewards, dones
+        return obses, next_obses, actions, rewards, dones
     
     def save_memory(self, filename):
         """
@@ -54,6 +58,9 @@ class ReplayBuffer(object):
         
         with open(filename + '/obses.npy', 'wb') as f:
             np.save(f, self.obses) 
+        
+        with open(filename + '/next_obses.npy', 'wb') as f:
+            np.save(f, self.next_obses) 
         
         with open(filename + '/actions.npy', 'wb') as f:
             np.save(f, self.actions) 
@@ -76,6 +83,9 @@ class ReplayBuffer(object):
         
         with open(filename + '/obses.npy', 'rb') as f:
             self.obses = np.load(f)
+        
+        with open(filename + '/next_obses.npy', 'rb') as f:
+            self.next_obses = np.load(f)
         
         with open(filename + '/actions.npy', 'rb') as f:
             self.actions = np.load(f)
